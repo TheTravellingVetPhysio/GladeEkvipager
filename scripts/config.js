3ef0eeb75c6be1b8cb6bfdc83a005a2432f8fe1b
TheTravellingVetPhysio <julietherold@hotmail.com>
2026-05-09 22:21:46 +0200

    Referencer til billedfiler rettet

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+const isLocal =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1";
+const ASSETS = isLocal ? "/resources" : "https://assets.gladeekvipager.dk";
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("img[data-src]").forEach((img) => {
+    img.src = `${ASSETS}/${img.dataset.src}`;
+  });
+});
+
+document.querySelectorAll("[data-bg]").forEach(el => {
+  el.style.backgroundImage = `url(${ASSETS}/${el.dataset.bg})`;
+});
